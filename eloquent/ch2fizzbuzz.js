91ffa6c23c383bf33233587eb6013530896594f4
ralphu <ralphutz@gmail.com>
2019-07-30 09:29:50 -0500

    prettier settings

--- eloquent/ch2fizzbuzz.js
+++ eloquent/ch2fizzbuzz.js
@@ -1,11 +1,11 @@
 for (let count = 1; count <= 100; count++) {
-  if (count % 5 == 0 && count % 3 == 0) {
-    console.log("FizzBuzz");
-  } else if (count % 3 == 0) {
-    console.log("Fizz");
-  } else if (count % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(count);
-  }
+    if (count % 5 == 0 && count % 3 == 0) {
+        console.log("FizzBuzz");
+    } else if (count % 3 == 0) {
+        console.log("Fizz");
+    } else if (count % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(count);
+    }
 }
