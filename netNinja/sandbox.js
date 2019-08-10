303ab738824dee8c2248b0ed4eb371e58502060e
ralphu <ralphutz@gmail.com>
2019-08-10 13:27:44 -0500

    arrow functions

--- netNinja/sandbox.js
+++ netNinja/sandbox.js
@@ -1,7 +1,11 @@
-const names = ["shaun", "mario", "luigi"];
+// const greet = () => "hello, world";
+// const result = greet();
 
-for (let i = 0; i < names.length; i++) {
-    //console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
-}
+const bill = (products, tax) => {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+};
+console.log(bill([10, 15, 30], 0.2));
