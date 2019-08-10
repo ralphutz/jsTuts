92faaa684813ba453a36633ba5a34b0b2df8e5bf
ralphu <ralphutz@gmail.com>
2019-08-10 12:30:50 -0500

    netNinja 1-3

--- netNinja/sandbox.js
+++ netNinja/sandbox.js
@@ -1,1 +1,7 @@
-console.log(1);
+const names = ["shaun", "mario", "luigi"];
+
+for (let i = 0; i < names.length; i++) {
+    //console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html);
+}
