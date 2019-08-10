8113818dc7a75bed16917d015fcc42d48d3b8497
ralphu <ralphutz@gmail.com>
2019-08-10 17:33:58 -0500

    method testing

--- netNinja/sandbox.js
+++ netNinja/sandbox.js
@@ -1,31 +1,16 @@
-// const greet = () => "hello, world";
-// const result = greet();
+let user = {
+    name: "crystal",
+    age: 30,
+    email: "test@cool.com",
+    location: "berlin",
+    blogs: ["why mac and cheese", "10 cool things"],
+    login: function() {
+        console.log("the user logged in");
+    },
+    logout: function() {
+        console.log("the user logged out");
+    }
+};
 
-// const bill = (products, tax) => {
-//     let total = 0;
-//     for (let i = 0; i < products.length; i++) {
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// };
-// console.log(bill([10, 15, 30], 0.2));
-
-// let people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-
-// const logPerson = (person, index) => {
-//     console.log(`${index} - hello ${person}`);
-// };
-
-// people.forEach(logPerson);
-const ul = document.querySelector(".people");
-
-const people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-
-let html = ``;
-
-people.forEach(person => {
-    html += `<li style="color: purple">${person}</li>`;
-});
-console.log(html);
-
-ul.innerHTML = html;
+user.login();
+user.logout();
