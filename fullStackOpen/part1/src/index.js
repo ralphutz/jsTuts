cc9bb69beeb0096386686bf0b3a164de81bbad61
ralphu <ralphutz@gmail.com>
2019-09-06 16:23:27 -0500

    part1.1

--- fullStackOpen/part1/src/index.js
+++ fullStackOpen/part1/src/index.js
@@ -1,26 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = props => {
+const App = () => {
+    const course = "Half Stack application development";
+    const part1 = "Fundamentals of React";
+    const exercises1 = 10;
+    const part2 = "Using props to pass data";
+    const exercises2 = 7;
+    const part3 = "State of a component";
+    const exercises3 = 14;
+
+    return (
+        <>
+            <Header course={course} />
+            <Content part={part1} exercise={exercises1} />
+            <Content part={part2} exercise={exercises2} />
+            <Content part={part3} exercise={exercises3} />
+            <Total exercises={exercises1 + exercises2 + exercises3} />
+        </>
+    );
+};
+
+const Header = props => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    );
+};
+
+const Content = props => {
     return (
         <div>
             <p>
-                Hello {props.name}, you are {props.age} years old
+                {props.part} {props.exercise}
             </p>
         </div>
     );
 };
 
-const App = () => {
-    const name = "Peter";
-    const age = 10;
-
+const Total = props => {
     return (
-        <>
-            <h1>Greetings</h1>
-            <Hello name="Maya" age={26 + 10} />
-            <Hello name={name} age={age} />
-        </>
+        <div>
+            <p>Number of exercises {props.exercises}</p>
+        </div>
     );
 };
 ReactDOM.render(<App />, document.getElementById("root"));
