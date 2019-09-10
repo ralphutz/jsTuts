7ba39a99426876ce7e3a14f04c07ed7fdc1c11f0
ralphu <ralphutz@gmail.com>
2019-09-10 15:50:28 -0500

    part1.4

--- fullStackOpen/part1/src/index.js
+++ fullStackOpen/part1/src/index.js
@@ -3,32 +3,36 @@ import ReactDOM from "react-dom";
 
 const App = () => {
     const course = "Half Stack application development";
-    const part1 = {
-        name: "Fundamentals of React",
-        exercises: 10
-    };
-    const part2 = {
-        name: "Using props to pass data",
-        exercises: 7
-    };
-    const part3 = {
-        name: "State of a component",
-        exercises: 14
-    };
+    const parts = [
+        {
+            name: "Fundamentals of React",
+            exercises: 10
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7
+        },
+        {
+            name: "State of a component",
+            exercises: 14
+        }
+    ];
 
     return (
         <>
             <Header course={course} />
             <Content
-                part1={part1.name}
-                exercise1={part1.exercises}
-                part2={part2.name}
-                exercise2={part2.exercises}
-                part3={part3.name}
-                exercise3={part3.exercises}
+                part1={parts[0].name}
+                exercise1={parts[0].exercises}
+                part2={parts[1].name}
+                exercise2={parts[1].exercises}
+                part3={parts[2].name}
+                exercise3={parts[2].exercises}
             />
             <Total
-                exercises={part1.exercises + part2.exercises + part3.exercises}
+                exercises={
+                    parts[0].exercises + parts[1].exercises + parts[2].exercises
+                }
             />
         </>
     );
